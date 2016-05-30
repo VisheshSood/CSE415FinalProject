270904b21f101c7f2d100a7c444131852eb4fcef
VISHESH SOOD <vsood@uw.edu>
2016-05-29 23:44:17 -0700

    added some new direction functions

--- rubik.py
+++ rubik.py
@@ -52,14 +52,16 @@ def createGoalState():
     for i in range(6):
         new = []
         for j in range(9):
-            new.append(i+1)
+            new.append(i + 1)
         state.append(new)
     GOAL_STATE = state
+
 
 def goalMessage():
     print("The Rubik's Cube has been solved!!")
     print("Final State: ")
     describeState(GOAL_STATE)
+
 
 def describeState(state):
     tiles = state[4]
@@ -78,11 +80,81 @@ def describeState(state):
     for i in range(0, 9, 3):
         print("      " + str(tiles[i]) + " " + str(tiles[i + 1]) + " " + str(tiles[i + 2]))
 
+
 def scramble():
     return None
 
+
+def move(action, s):
+    # act = actions.get(action)
+    act(s)
+
+#going from 5 to 3, reverse order of tiles
+#reverse back when going
+def left(s):
+    indices = [0, 1, 2]
+    rotation = [5, 3, 4, 1]
+    last_update = [s[1][0], s[1][3], s[1][6]]
+    for i in range(3, 0, -1):
+        if rotation[i] == 3:
+            s[rotation[i]][8] = s[rotation[i - 1]][0]
+            s[rotation[i]][5] = s[rotation[i - 1]][3]
+            s[rotation[i]][2] = s[rotation[i - 1]][6]
+        if rotation[i] == 4:
+            s[rotation[i]][0] = s[rotation[i - 1]][8]
+            s[rotation[i]][3] = s[rotation[i - 1]][5]
+            s[rotation[i]][6] = s[rotation[i - 1]][2]
+        else:
+            s[rotation[i]][0] = s[rotation[i - 1]][0]
+            s[rotation[i]][3] = s[rotation[i - 1]][3]
+            s[rotation[i]][6] = s[rotation[i - 1]][6]
+    s[5][0] = last_update[0]
+    s[5][3] = last_update[1]
+    s[5][6] = last_update[2]
+    return s
+
+
+def right(s):
+    return 0
+
+def up(s):
+    indices = [0, 1, 2]
+    rotation = [3, 2, 1, 0]
+    last_update = [s[0][0], s[0][1], s[0][2]]
+    for i in range(3, 0, -1):
+        s[rotation[i]][0] = s[rotation[i - 1]][0]
+        s[rotation[i]][1] = s[rotation[i - 1]][1]
+        s[rotation[i]][2] = s[rotation[i - 1]][2]
+    s[3][0] = last_update[0]
+    s[3][1] = last_update[1]
+    s[3][2] = last_update[2]
+    return s
+
+
+def down(s):
+    indices = [6, 7, 8]
+    rotation = [1, 2, 3, 0]
+    last_update = [s[0][6], s[0][7], s[0][8]]
+    for i in range(3, 0, -1):
+        s[rotation[i]][6] = s[rotation[i - 1]][6]
+        s[rotation[i]][7] = s[rotation[i - 1]][7]
+        s[rotation[i]][8] = s[rotation[i - 1]][8]
+    s[1][6] = last_update[0]
+    s[1][7] = last_update[1]
+    s[1][8] = last_update[2]
+    return s
+
+
+def front(s):
+    return 0
+
+def back(s):
+    return 0
+
 createGoalState()
 describeState(INITIAL_STATE)
+print()
+describeState(left(INITIAL_STATE))
 
 
 
