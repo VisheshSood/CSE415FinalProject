82dc1fc7f73afdb3f3d4250456bf86d013859a94
VISHESH SOOD <vsood@uw.edu>
2016-05-30 02:17:44 -0700

    front directions work!

--- rubik.py
+++ rubik.py
@@ -17,7 +17,7 @@ PROBLEM_DESC = \
 # SIDES = ["Left", "Front", "Right", "Back", "Up", "Down"]
 INITIAL_STATE = [[5, 2, 5, 2, 1, 1, 2, 1, 1], [3, 3, 4, 5, 2, 5, 5, 3, 5],
                  [6, 4, 6, 3, 3, 4, 3, 3, 4], [2, 1, 1, 6, 4, 6, 6, 1, 6],
-                 [4, 4, 1, 5, 5, 5, 4, 4, 1], [2, 6, 2, 2, 6, 2, 3, 6, 3]]
+                 [4, 4, 3, 5, 5, 5, 4, 4, 1], [2, 6, 2, 2, 6, 2, 3, 6, 3]]
 GOAL_STATE = None
 
 
@@ -89,8 +89,9 @@ def move(action, s):
     # act = actions.get(action)
     act(s)
 
-#going from 5 to 3, reverse order of tiles
-#reverse back when going
+
+# going from 5 to 3, reverse order of tiles
+# reverse back when going
 def left(s):
     indices = [0, 1, 2]
     rotation = [5, 3, 4, 1]
@@ -115,7 +116,27 @@ def left(s):
 
 
 def right(s):
-    return 0
+    indices = [0, 1, 2]
+    rotation = [4, 3, 5, 1]
+    last_update = [s[1][2], s[1][5], s[1][8]]
+    for i in range(3, 0, -1):
+        if rotation[i] == 3:
+            s[rotation[i]][6] = s[rotation[i - 1]][2]
+            s[rotation[i]][3] = s[rotation[i - 1]][5]
+            s[rotation[i]][0] = s[rotation[i - 1]][8]
+        if rotation[i] == 5:
+            s[rotation[i]][2] = s[rotation[i - 1]][6]
+            s[rotation[i]][5] = s[rotation[i - 1]][3]
+            s[rotation[i]][8] = s[rotation[i - 1]][0]
+        else:
+            s[rotation[i]][2] = s[rotation[i - 1]][2]
+            s[rotation[i]][5] = s[rotation[i - 1]][5]
+            s[rotation[i]][8] = s[rotation[i - 1]][8]
+    s[4][2] = last_update[0]
+    s[4][5] = last_update[1]
+    s[4][8] = last_update[2]
+    return s
+
 
 def up(s):
     indices = [0, 1, 2]
@@ -146,15 +167,75 @@ def down(s):
 
 
 def front(s):
-    return 0
+    last_update = [s[5][0], s[5][1], s[5][2]]
+
+    # Get values from 2 and put into 5
+    s[5][0] = s[2][6]
+    s[5][1] = s[2][3]
+    s[5][2] = s[2][0]
+
+    # Get values from 4 and put into 2
+    s[2][0] = s[4][6]
+    s[2][3] = s[4][7]
+    s[2][6] = s[4][8]
+
+    # Get values from 0 and put into 4
+    s[4][8] = s[0][2]
+    s[4][7] = s[0][5]
+    s[4][6] = s[0][8]
+
+    # Get values from 5 and put into 0
+    s[0][2] = last_update[0]
+    s[0][5] = last_update[1]
+    s[0][8] = last_update[2]
+
+    tempArray = []
+
+    for i in range(3):
+        val1 = s[1][i * 3]
+        val2 = s[1][(i * 3) + 1]
+        val3 = s[1][(i * 3) + 2]
+        tempArray.append([val1, val2, val3])
+
+    rot2(tempArray)
+
+    list_to_update = []
+    for i in range(3):
+        for j in range(3):
+            list_to_update.append(tempArray[i][j])
+
+    s[1] = list_to_update
+
+    return s
+
+
+def rot2(a):
+    n = 3
+    c = (n + 1) / 2
+    f = n / 2
+    for x in range(2):
+        for y in range(1):
+            a[x][y] = a[x][y] ^ a[n - 1 - y][x]
+            a[n - 1 - y][x] = a[x][y] ^ a[n - 1 - y][x]
+            a[x][y] = a[x][y] ^ a[n - 1 - y][x]
+
+            a[n - 1 - y][x] = a[n - 1 - y][x] ^ a[n - 1 - x][n - 1 - y]
+            a[n - 1 - x][n - 1 - y] = a[n - 1 - y][x] ^ a[n - 1 - x][n - 1 - y]
+            a[n - 1 - y][x] = a[n - 1 - y][x] ^ a[n - 1 - x][n - 1 - y]
+
+            a[n - 1 - x][n - 1 - y] = a[n - 1 - x][n - 1 - y] ^ a[y][n - 1 - x]
+            a[y][n - 1 - x] = a[n - 1 - x][n - 1 - y] ^ a[y][n - 1 - x]
+            a[n - 1 - x][n - 1 - y] = a[n - 1 - x][n - 1 - y] ^ a[y][n - 1 - x]
+
 
 def back(s):
     return 0
 
+
 createGoalState()
 describeState(INITIAL_STATE)
 print()
-describeState(left(INITIAL_STATE))
+describeState(front(INITIAL_STATE))
 
 
 
